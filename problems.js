391408f6641d01c45aa1391716f90faf3da5945f
byunn90 <kayhanturk90@Live.com>
2024-11-28 19:49:28 +1100

    question

--- problems.js
+++ problems.js
@@ -114,14 +114,69 @@ Constraints:
 0 <= functions.length <= 1000
 all functions accept and return a single integer
 */
-var compose = function (functions) {
-  return function (x) {
-    return functions.reduceRight((acc, fn) => fn(acc), x);
-  };
-};
+// var compose = function (functions) {
+//   return function (x) {
+//     return functions.reduceRight((acc, fn) => fn(acc), x);
+//   };
+// };
 
-// Example usage
-const fn = compose([(x) => x + 1, (x) => x * x, (x) => 2 * x]);
-console.log(fn(4));
+// // Example usage
+// const fn = compose([(x) => x + 1, (x) => x * x, (x) => 2 * x]);
+// console.log(fn(4));
 
 // console.log(compose(fn(4)));
+/*
+2634. Filter Elements from Array
+Attempted
+Easy
+Companies
+Hint
+Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
+
+The fn function takes one or two arguments:
+
+arr[i] - number from the arr
+i - index of arr[i]
+filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
+
+Please solve it without the built-in Array.filter method.
+
+ 
+
+Example 1:
+
+Input: arr = [0,10,20,30], fn = function greaterThan10(n) { return n > 10; }
+Output: [20,30]
+Explanation:
+const newArray = filter(arr, fn); // [20, 30]
+The function filters out values that are not greater than 10
+Example 2:
+
+Input: arr = [1,2,3], fn = function firstIndex(n, i) { return i === 0; }
+Output: [1]
+Explanation:
+fn can also accept the index of each element
+In this case, the function removes elements not at index 0
+Example 3:
+
+Input: arr = [-2,-1,0,1,2], fn = function plusOne(n) { return n + 1 }
+Output: [-2,0,1,2]
+Explanation:
+Falsey values such as 0 should be filtered out
+ 
+
+Constraints:
+
+0 <= arr.length <= 1000
+-109 <= arr[i] <= 109
+*/
+var filter = function (arr, fn) {
+  const newArray = [arr];
+  newArray.filter((x) x > 10);
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element);
+  }
+};
+filter([1, 2, 3, 4, 5], (x) => x > 2);
+const fn = (x) => x > 10;
